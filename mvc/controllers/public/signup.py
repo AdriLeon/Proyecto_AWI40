42ccd4603aee1e1a1cdcc923bee1e386f0f02e85
AdriLeon <51243138+AdriLeon@users.noreply.github.com>
2022-04-09 00:47:33 -0500

    signup(que no sera implementado) funcionando

--- mvc/controllers/public/signup.py
+++ mvc/controllers/public/signup.py
@@ -19,21 +19,25 @@ class Signup:
             phone = formulario.phone
             email = formulario.email
             password = formulario.password
+            nivel = 'Operador'
+            status = 'Enable'
             user= auth.create_user_with_email_and_password(email, password)# nos permitira crear nuevo usuario y contraseña
             print(user['localId']) 
             data = {# se crea una variable, en ella introducirmos los valores del formulario en formato json (un diccionario)
                 "name": name,
                 "phone": phone,
-                "email": email
+                "email": email,
+                "nivel": nivel,
+                "status": status
             }
             results = db.child("users").child(user['localId']).set(data)#aqui almacenaremos los datos en la pase de datos, user para el nombre de la tabla, de ahi generamos otra rama que sera la de la localId
             web.setcookie('localId', user['localId'], 3600)
             print("localId: ", web.cookies().get('localId'))
-            return web.seeother("login") # redirecciona a la pagina de login
+            return web.seeother("/login") # redirecciona a la pagina de login
         except Exception as error:
             format = json.loads(error.args[1])
             error = format['error']
             message = error['message']
             print("Error Login.POST: {}".format(message))
             web.setcookie('localId', '', 3600)
-            return render.login(message)
+            return render.signup(message)
