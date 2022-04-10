0c7af559cc38e81f0473b277a78d2e969486f05d
AdriLeon <51243138+AdriLeon@users.noreply.github.com>
2022-04-09 22:57:47 -0500

    register realizado aun sin funcionar

---
+++ mvc/controllers/admin/register.py
@@ -0,0 +1,44 @@
+import web
+import pyrebase
+import firebase_config as token 
+import app as app
+import json
+
+render = web.template.render("mvc/views/admin/")
+
+class Register:
+    def GET(self):
+        return render.register()
+    def POST(self):
+        try:
+            firebase = pyrebase.initialize_app(token.firebaseConfig)
+            auth = firebase.auth()
+            db = firebase.database()
+            formulario = web.input()
+            name = formulario.name
+            phone = formulario.phone
+            email = formulario.email
+            password = '      '
+            nivel = formulario.nivel
+            status = formulario.status
+            user= auth.create_user_with_email_and_password(email, password)# nos permitira crear nuevo usuario y contraseña
+            print(user['localId']) 
+            data = {# se crea una variable, en ella introducirmos los valores del formulario en formato json (un diccionario)
+                "name": name,
+                "phone": phone,
+                "email": email,
+                "nivel": nivel,
+                "status": status
+            }
+            results = db.child("users").child(user['localId']).set(data)#aqui almacenaremos los datos en la pase de datos, user para el nombre de la tabla, de ahi generamos otra rama que sera la de la localId
+            web.setcookie('localId', user['localId'], 3600)
+            print("localId: ", web.cookies().get('localId'))
+            message = "Usuario registrado."
+            return render.register(message) # redirecciona a la pagina de login
+        except Exception as error:
+            format = json.loads(error.args[1])
+            error = format['error']
+            message = error['message']
+            print("Error Login.POST: {}".format(message))
+            web.setcookie('localId', '', 3600)
+            return render.register(message)
